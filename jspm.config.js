a728aab701e7936258db4ff89c185249c972b3a1
Jake Niemiec <jake.j.niemiec@gmail.com>
2016-05-31 10:26:58 -0500

    feat: init push

---
+++ jspm.config.js
@@ -0,0 +1,49 @@
+SystemJS.config({
+  paths: {
+    "github:": "jspm_packages/github/",
+    "npm:": "jspm_packages/npm/",
+    "jspmsandbox/": "src/"
+  },
+  browserConfig: {
+    "baseURL": "/"
+  },
+  devConfig: {
+    "map": {
+      "plugin-typescript": "github:frankwallis/plugin-typescript@4.0.16",
+      "os": "github:jspm/nodelibs-os@0.2.0-alpha"
+    },
+    "packages": {
+      "github:frankwallis/plugin-typescript@4.0.16": {
+        "map": {
+          "typescript": "npm:typescript@1.8.10"
+        }
+      },
+      "github:jspm/nodelibs-os@0.2.0-alpha": {
+        "map": {
+          "os-browserify": "npm:os-browserify@0.2.1"
+        }
+      }
+    }
+  },
+  transpiler: "plugin-typescript",
+  packages: {
+    "jspmsandbox": {
+      "main": "jspmsandbox.js",
+      "meta": {
+        "*.js": {
+          "loader": "plugin-typescript"
+        }
+      }
+    }
+  }
+});
+
+SystemJS.config({
+  packageConfigPaths: [
+    "github:*/*.json",
+    "npm:@*/*.json",
+    "npm:*.json"
+  ],
+  map: {},
+  packages: {}
+});
---
+++ src/otherfile.js
@@ -0,0 +1,14 @@
+System.register([], function(exports_1, context_1) {
+    "use strict";
+    var __moduleName = context_1 && context_1.id;
+    function test() {
+        return true;
+    }
+    exports_1("test", test);
+    return {
+        setters:[],
+        execute: function() {
+        }
+    }
+});
+//# sourceMappingURL=otherfile.js.map
